6b274318acf1dfc34883cc199a196c7e7c7c34f5
Ayush Badola <ayush.badola.04@gmail.com>
2025-03-02 20:34:58 +0530

    feat: uI for signin and sign up

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import { SignUpSchema } from '@/zod-schemas/sign-up-schema';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from 'zod';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Image from 'next/image';
+import Link from "next/link";
+import axios from 'axios'; 
+import { useMutation } from '@tanstack/react-query';
+import {signupUser} from '@/helpers/auth';
+
+
+export default function SignUpPage() {
+  const {  status } = useSession();
+  const router = useRouter();
+  const mutation = useMutation({
+    mutationFn : signupUser, 
+    onSuccess : ()=> setTimeout(() => {
+      router.replace("/signin");
+    }, 2000),
+    onError : (error) =>{
+      if(axios.isAxiosError(error)) 
+        console.log(error.response?.data)
+      else
+        console.log(error); 
+    }
+  })
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.replace('/protected-page')
+    }
+  }, [status, router]);
+
+
+  const form = useForm({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      email: "",
+      password: "", 
+      name : "  "
+    }
+  });
+
+
+  
+  async function onSubmit(formdata: z.infer<typeof SignUpSchema>) {
+    mutation.mutate(formdata);
+  }
+
+  return (
+    <div className='bg-black h-screen w-screen flex items-center justify-center text-white'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="bg-black text-white border border-white p-8 rounded-xl w-96 space-y-6">
+          <h1 className='font-bold text-2xl text-center'>Sign up</h1>
+
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input className="bg-black border-white text-white" type='text' placeholder='youname' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input className="bg-black border-white text-white" type='email'placeholder='email ' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input className="bg-black border-white text-white" type='password' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className='w-full' disabled={mutation.isPending}> {mutation.isPending ? "Submitting.... "  : "Submit"}</Button>
+          
+          {mutation.isError && <p className="text-red-500 text-sm">{mutation.error?.message || "Signup failed."}</p>}
+
+          {mutation.isSuccess && <div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs text-white ">
+    Redirecting...
+  </div>
+}
+
+          <div className="flex items-center justify-center space-x-2">
+            <div className="w-full h-px bg-white"></div>
+            <p className="text-white text-sm">OR</p>
+            <div className="w-full h-px bg-white"></div>
+          </div>
+
+          <div className='flex flex-col space-y-3'>
+            <Button onClick={()=> signIn('google')}>
+              <Image src="/google-icon.png" alt="Google" width={20} height={20} />
+              Sign in with Google
+            </Button>
+            <Button onClick={()=>signIn('github')} >
+              <Image src="/github-icon.png" alt="GitHub" width={20} height={20} />
+              Sign in with GitHub
+            </Button>
+          </div>
+
+          <p className='text-center'>Already part of us , <Link href='/signin' className='text-blue-200'>Sign in</Link></p>
+        </form>
+      </Form>
+    </div>
+  );
+}
